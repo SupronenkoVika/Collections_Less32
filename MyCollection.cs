8250a2380b30c4d6ac6d6c826b510c13a79c8d7c
Виктория Супроненко <viktoryia.supronenko@gmail.com>
2023-02-28 16:10:55 +0300

    Add project files.

---
+++ MyCollection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace Collections_Less32
+{
+    public class MyCollection : IEnumerable
+    {
+        private int[] _monthNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyEnumerator(_monthNumbers);
+        }
+    }
+
+    public class MyEnumerator : IEnumerator
+    {
+        private int index = -1;
+        private int[] _monthNumbers;
+
+        public MyEnumerator(int[] numbers)
+        {
+            _monthNumbers = numbers;
+        }
+        public bool MoveNext()
+        {
+            if (index < _monthNumbers.Length)
+                index++;
+
+            return (!(index == _monthNumbers.Length));
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if (index < 0 || index == _monthNumbers.Length)
+                    throw new InvalidOperationException();
+                return _monthNumbers[index];
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Collections_Less32;
+
+MyCollection collection = new MyCollection();
+
+foreach (var var in collection)
+{
+    Console.Write($"M:{var}" + "  ");
+}
+
+Console.ReadLine();
